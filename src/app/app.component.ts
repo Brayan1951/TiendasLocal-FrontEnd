3d2d11f1d10857aeaa5b186bed8160047878e059
Brayan1951 <brayanc1951@gmail.com>
2022-01-25 12:43:57 -0500

    arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface MenuIten {
+  texto: string;
+  ruta: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tiendas-frontEnd';
+  menuItems: MenuIten[] = [
+    {
+      texto: 'Tienda',
+      ruta: './tiendas/pages/home'
+    },
+    {
+      texto: 'Productos',
+      ruta: './productos/pages/lista'
+    }
+  ]
 }
